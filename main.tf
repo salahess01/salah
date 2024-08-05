7c2a5fec360d8897db6f225f9f29bfc0a49a66a3
salahess01 <t3limtv@gmail.com>
2024-08-05 13:46:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     virtualbox = {
-      source = "terra-farm/virtualbox"
-#      version = "0.2.2"
+      source = "shekeriev/virtualbox"
+#      version = "0.0.4"
     }
   }
 }
