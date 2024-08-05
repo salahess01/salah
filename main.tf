698b9678795789e73deb7af6cc22917b749ff364
salahess01 <t3limtv@gmail.com>
2024-08-05 13:11:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,28 +2,31 @@ terraform {
   required_providers {
     virtualbox = {
       source = "terra-farm/virtualbox"
-      version = "0.2.2" # Use the latest version available
+      version = "0.2.1"
     }
   }
 }
 
-provider "virtualbox" {
-  # Configuration options (if any)
-}
+# There are currently no configuration options for the provider itself.
 
-resource "virtualbox_vm" "my_vm" {
-  name      = "my-vm"
-  os_type   = "Ubuntu_64"
+resource "virtualbox_vm" "node" {
+  count     = 2
+  name      = format("node-%02d", count.index + 1)
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 2
-  memory    = 1024
-  vram      = 12
+  memory    = "512 mib"
+  user_data = file("${path.module}/user_data")
 
   network_adapter {
     type           = "hostonly"
-    host_interface = "vboxnet0"
+    host_interface = "vboxnet1"
   }
+}
 
-  disk {
-    size = 10240 # Size in MB
-  }
+output "IPAddr" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+}
+
+output "IPAddr_2" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
 }
