4a1c11dbc6a01e7fbb3f23a6d7e82626424ca332
salahess01 <t3limtv@gmail.com>
2024-08-05 13:53:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,24 +9,19 @@ terraform {
 
 # There are currently no configuration options for the provider itself.
 
-resource "virtualbox_vm" "node" {
-  count     = 2
-  name      = format("node-%02d", count.index + 1)
-  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+resource "virtualbox_vm" "my_vm" {
+  name      = "my-vm"
+  os_type   = "Ubuntu_64"
   cpus      = 2
-  memory    = "512 mib"
-  user_data = file("${path.module}/user_data")
+  memory    = 1024
+  vram      = 12
 
   network_adapter {
     type           = "hostonly"
-    host_interface = "vboxnet1"
+    host_interface = "vboxnet0"
   }
-}
 
-output "IPAddr" {
-  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
-}
-
-output "IPAddr_2" {
-  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
+  disk {
+    size = 10240 # Size in MB
+  }
 }
